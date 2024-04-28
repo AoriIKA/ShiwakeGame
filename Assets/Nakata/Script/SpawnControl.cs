2a91ed2d28c9cd6000838994ffad0f609c6bc2a4
rinne9029 <22051915@d.anabuki-college.net>
2024-04-28 21:42:06 +0900

    追加 デモシーン用の順番に生成する関数とメインゲーム用にランダムに生成する関数とゲームマネージャーから生成を切り替える関数を追加

--- Assets/Nakata/Script/SpawnControl.cs
+++ Assets/Nakata/Script/SpawnControl.cs
@@ -6,13 +6,25 @@ public class SpawnControl : MonoBehaviour
 {
     [SerializeField, Header("リスポーン時間")]
     private float SpawnTime;
+
     //現在の時間
-    private float time;
-    //生成する個数
+    private float time = 0;
+
+    //生成を行うフラグ
+    private bool Generate = true;
+
     [SerializeField, Header("生成する数")]
     private int count;
+
     [SerializeField, Header("生成するゲームオブジェクト")]
     private GameObject[] Obj;
+
+    //生成するオブジェクトを決める数
+    private int number = 0;
+    
+    //チュートリアルとメインを切り替えるフラグ
+    private bool ChangeGame = false;
+
     void Start()
     {
 
@@ -20,12 +32,32 @@ public class SpawnControl : MonoBehaviour
 
     void Update()
     {
-        //個数が0以下なら生成停止
-        if (count > 0) Spawn();
+        if(Input.GetKeyDown(KeyCode.Z))
+        {
+            ChangeGame = true;
+            Generate = IsGenerate(Generate);
+        }
+
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            Generate = IsGenerate(Generate);
+        }
+
+        if(!ChangeGame)
+        {
+            TutorialSpawn();
+        }
+        else
+        {
+            if (count > 0) MainSpawn();
+        }
+
     }
 
-    private void Spawn()
+    private void TutorialSpawn()
     {
+        if (!Generate) return;
+
         if (SpawnTime > time)
         {
             //時間を加算
@@ -33,12 +65,40 @@ public class SpawnControl : MonoBehaviour
         }
         else
         {
-            //オブジェクト生成
-            Instantiate(Obj[0],this.transform.position, Quaternion.identity);
+            //オブジェクト配列順に生成
+            Instantiate(Obj[number % Obj.Length], this.transform.position, Quaternion.identity);
+            //タイマーリセット
+            time = 0;
+            //次のオブジェクトを出す
+            number++;
+        }
+        
+    }
+
+    private void MainSpawn()
+    {
+        if (!Generate) return;
+
+        if (SpawnTime > time)
+        {
+            //時間を加算
+            time += Time.deltaTime;
+        }
+        else
+        {
+            //オブジェクトランダムに生成
+            Instantiate(Obj[Random.Range(0, Obj.Length)], this.transform.position, Quaternion.identity);
             //タイマーリセット
             time = 0;
             //残り個数を減らす
             count--;
         }
+    }
+
+    public bool IsGenerate(bool flag)
+    {
+        //フラグの切り替え
+        flag = !flag;
+        return flag;
     }
 }
